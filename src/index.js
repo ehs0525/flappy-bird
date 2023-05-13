cd784f752b647c6dcbe434791ee1d944883e5dbc
엄희성 <ehs0525@naver.com>
2023-05-14 00:14:42 +0900

    start, key 이벤트 리스너 달기

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./style.css";
+
+const $score = document.querySelector(".score");
+const $start_btn = document.querySelector(".start_btn");
+const $game_area = document.querySelector(".game_area");
+const $game_message = document.querySelector(".game_message");
+
+const start = () => {
+  console.log("game start");
+  $game_message.classList.add("hide");
+  $start_btn.classList.add("hide");
+};
+const onKeyDown = () => {
+  console.log("on");
+};
+const onKeyUp = () => {
+  console.log("off");
+};
+
+$start_btn.addEventListener("click", start);
+$game_message.addEventListener("click", start);
+document.addEventListener("keydown", onKeyDown);
+document.addEventListener("keyup", onKeyUp);
