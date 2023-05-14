9d809df2085e5461549c7c5769aeb25ce13f0729
엄희성 <ehs0525@naver.com>
2023-05-14 21:27:18 +0900

    Update: bird -> mercy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import "./style.css";
 
 const $score = document.querySelector(".score");
+const $btn_container = document.querySelector(".btn_container");
 const $start_btn = document.querySelector(".start_btn");
 const $game_area = document.querySelector(".game_area");
 const $game_message = document.querySelector(".game_message");
 let keys = {};
 
-let $bird = document.createElement("div");
+let $mercy = document.createElement("div");
 let $wing = document.createElement("div");
 let player = {
   x: 0,
@@ -22,7 +23,7 @@ const playGame = () => {
     move = true;
   } else if (
     keys.ArrowDown &&
-    player.x < $game_area.offsetHeight - $bird.offsetHeight
+    player.x < $game_area.offsetHeight - $mercy.offsetHeight
   ) {
     player.x += player.speed;
     move = true;
@@ -31,7 +32,7 @@ const playGame = () => {
     move = true;
   } else if (
     keys.ArrowRight &&
-    player.y < $game_area.offsetWidth - $bird.offsetWidth
+    player.y < $game_area.offsetWidth - $mercy.offsetWidth
   ) {
     player.y += player.speed;
     move = true;
@@ -42,25 +43,29 @@ const playGame = () => {
   if (move) {
     $wing.pos = $wing.pos === 15 ? 25 : 15;
     $wing.style.top = $wing.pos + "px";
+    $wing.style.left = "-" + $wing.pos + "px";
+  } else {
+    $wing.style.top = "15px";
+    $wing.style.left = "-20px";
   }
-  $bird.style.top = player.x + "px";
-  $bird.style.left = player.y + "px";
+  $mercy.style.top = player.x + "px";
+  $mercy.style.left = player.y + "px";
   $score.innerText = "점수 : " + ++player.score;
   window.requestAnimationFrame(playGame);
 };
 const start = () => {
   console.log("game start");
   $game_message.classList.add("hide");
-  $start_btn.classList.add("hide");
+  $btn_container.classList.add("hide");
 
-  $bird.setAttribute("class", "bird");
+  $mercy.setAttribute("class", "mercy");
   $wing.setAttribute("class", "wing");
   $wing.pos = 15;
   $wing.style.top = $wing.pos + "px";
-  $bird.appendChild($wing);
-  $game_area.appendChild($bird);
-  player.x = $bird.offsetTop;
-  player.y = $bird.offsetLeft;
+  $mercy.appendChild($wing);
+  $game_area.appendChild($mercy);
+  player.x = $mercy.offsetTop;
+  player.y = $mercy.offsetLeft;
 
   window.requestAnimationFrame(playGame);
 };
